33619ff74977f218c74990358e7a8ccaf6ae30be
kwadra <git@demosoft.org>
2024-01-20 14:49:33 -0700

    Add files

---
+++ README.md
@@ -0,0 +1,1 @@
+Connect to dump1090 basestation port and persist messages to a REDIS data store
---
+++ flight_aware_redis.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import sys, os.path
+from config import *
+#sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+import redis
+import logging
+import py1090 
+import datetime
+logging.basicConfig(level=logging.INFO)
+logging.info("Connecting to %s:%d", redis_host,redis_port)
+r = redis.Redis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True, socket_timeout=2.0)
+
+def _dump_bool(value):
+    if value == True:
+        return 1
+    else:
+        return 0
+
+def to_record(message):
+    return_dict = {}
+    for k,v in message.__dict__.items():
+        if isinstance(v, datetime.datetime):
+            v = str(v)
+        if v is None:
+            continue
+        if isinstance(v, bool):
+            v = _dump_bool(v)
+        return_dict[k] = v
+    return return_dict
+
+def record_positions_to_redis(redis_client):
+    with py1090.Connection(host=fa_host) as connection:
+        for line in connection:
+            message = py1090.Message.from_string(line)
+            redis_client.hset(message.hexident, mapping=to_record(message))
+            logging.info("Updating %s", message.hexident)
+
+if __name__ == "__main__":
+    while True:
+        try:
+            record_positions_to_redis(r)
+        except Exception as e:
+            pass
