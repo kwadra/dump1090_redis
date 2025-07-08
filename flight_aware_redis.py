c08c3c167437d597e8bb9665d63f916baebeda90
kwadra <git@demosoft.org>
2025-07-07 20:18:26 -0700

    Add mqtt feature for nearby aircraft

--- flight_aware_redis.py
+++ flight_aware_redis.py
@@ -5,6 +5,9 @@ import redis
 import logging
 import py1090 
 import datetime
+import paho.mqtt.publish as publish
+from py1090.helpers import distance_between, bearing_between
+
 FORMAT = '%(asctime)s %(levelname)-8s %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 logging.info("Connecting to %s:%d", redis_host,redis_port)
@@ -29,12 +32,22 @@ def to_record(message):
         return_dict[k] = v
     return return_dict
 
+def publish_rec(message):
+    logging.info("queue message %s on %s", message, mqtt_topic_name)
+    publish.single(mqtt_topic_name, str(message), hostname=mqtt_host)
+
 def record_positions_to_redis(redis_client):
     with py1090.Connection(host=fa_host) as connection:
         for line in connection:
             message = py1090.Message.from_string(line)
+            if message.latitude and message.longitude:
+                distance = distance_between(home_lat, home_long, message.latitude, message.longitude) * 0.000621371
+                message.distance = distance
+                if distance <= mqtt_distance_max:
+                    # and message.callsign:
+                    logging.info("Updating %s %s ", message.hexident, message.callsign)
+                    publish_rec( message.callsign)
             redis_client.hset(message.hexident, mapping=to_record(message))
-            logging.info("Updating %s", message.hexident)
 
 if __name__ == "__main__":
     while True:
