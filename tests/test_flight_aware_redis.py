e45b3883e02fd4420163e94c85d1041a54c798f8
kwadra <git@demosoft.org>
2025-07-12 15:13:17 -0700

    fix pytest again

--- tests/test_flight_aware_redis.py
+++ tests/test_flight_aware_redis.py
@@ -1,14 +1,11 @@
+import py1090
+
+from flight_aware_redis import to_record
 
 
 def test_to_record():
-    from flight_aware_redis import to_record, FLIGHTS
-    from flight_aware_redis import py1090
-
-    # Create a mock flight message
+# Create a mock flight message
     message = py1090.Message.from_string("MSG,3,111,11111,3C49CC,111111,2015/05/01,17:06:55.370,2015/05/01,17:06:55.326,,24400,,,50.65931,6.67709,,,,,,0")
-
-    # Add the message to FLIGHTS
-    FLIGHTS.add(message)
 
     # Convert the message to a record
     record = to_record(message)
